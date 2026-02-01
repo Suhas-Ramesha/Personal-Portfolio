568042374b5b0d019f0359697db1bab133579e0c
Suhas R <rsuhas319@gmail.com>
2026-02-01 18:19:46 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Suhas-Ramesha",
   resume:
-    "https://drive.google.com/file/d/1KLU3L0TlQPESiz0lYX3gEC6geOfj00kn/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1KLU3L0TlQPESiz0lYX3gEC6geOfj00kn/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/suhas-ramesha/",
   twitter: "",
   insta: "",
